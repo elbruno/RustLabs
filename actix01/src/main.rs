6cf568a3e4788cb33efbed905b4dd0f30f6ead08
El Bruno <bcapuano@gmail.com>
2023-01-16 10:05:37 -0500

    cmd line demo webserver demo

--- actix01/src/main.rs
+++ actix01/src/main.rs
@@ -3,7 +3,8 @@
    Author      : Bruno Capuano
    Create Time : 2023 January
    Change Log  :
-   - Demo reading lines from a cmd input
+   - Demo working with Actix as webserver
+   - Source Demo: https://actix.rs/docs/getting-started
    
    The MIT License (MIT)
    Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -23,10 +24,31 @@
    THE SOFTWARE.
 */
 
-fn main(){
-    let mut line = String::new();
-    println!("Enter your name :");
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-    println!("Hello , {}", line);
-    println!("no of bytes read , {}", b1);
- }
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
