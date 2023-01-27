ad48abd0a20761bce9f89838cd25d15437efc953
El Bruno <bcapuano@gmail.com>
2023-01-27 14:39:04 -0500

    correct call to openai apis

--- openai02/src/main.rs
+++ openai02/src/main.rs
@@ -1,95 +1,52 @@
+use std::collections::HashMap;
 use reqwest;
 use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
 use std::env;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct ExternalUrls {
-    spotify: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Artist {
-    name: String,
-    external_urls: ExternalUrls,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Album {
-    name: String,
-    artists: Vec<Artist>,
-    external_urls: ExternalUrls,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Track {
-    name: String,
-    href: String,
-    popularity: u32,
-    album: Album,
-    external_urls: ExternalUrls,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Items<T> {
-    items: Vec<T>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct APIResponse {
-    tracks: Items<Track>,
-}
-
-fn print_tracks(tracks: Vec<&Track>) {
-    for track in tracks {
-        println!("🔥 {}", track.name);
-        println!("💿 {}", track.album.name);
-        println!(
-            "🕺 {}",
-            track
-                .album
-                .artists
-                .iter()
-                .map(|artist| artist.name.to_string())
-                .collect::<String>()
-        );
-        println!("🌎 {}", track.external_urls.spotify);
-        println!("---------")
-    }
-}
-
 #[tokio::main]
 async fn main() {
+    let data = r#"
+    {
+        "model": "text-davinci-003",
+        "prompt": "Describe the Rust programming language",
+        "temperature": 0.9,
+        "max_tokens": 1024
+    }"#;
+
+    let mut map = HashMap::new();
+    map.insert("model", "text-davinci-003");
+    map.insert("prompt", "describe rust programming language");
+    map.insert("temperature", "0.9");
+    map.insert("max_tokens", "1024");
+
     let args: Vec<String> = env::args().collect();
-    let search_query = &args[1];
-    let auth_token = &args[2];
-    let url = format!(
-        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
-        query = search_query
-    );
+    let auth_token = &args[1];
+
+    println!("Auth token: {}", auth_token);
+    let bearer_auth = format!("Bearer {}", auth_token);
+    println!("Bearer Auth token: {}", bearer_auth);
+
+    let url = "https://api.openai.com/v1/completions".to_string();
     let client = reqwest::Client::new();
     let response = client
-        .get(url)
-        .header(AUTHORIZATION, format!("Bearer {}", auth_token))
+        .post(url)
+        .header(ACCEPT, "*/*")
+        .header(AUTHORIZATION, &bearer_auth)
         .header(CONTENT_TYPE, "application/json")
-        .header(ACCEPT, "application/json")
+        .body(data)
         .send()
         .await
         .unwrap();
     match response.status() {
         reqwest::StatusCode::OK => {
-            match response.json::<APIResponse>().await {
-                Ok(parsed) => print_tracks(parsed.tracks.items.iter().collect()),
-                Err(_) => println!("Hm, the response didn't match the shape we expected."),
-            };
+            println!("🔥 Response: {:#?}", response);
         }
         reqwest::StatusCode::UNAUTHORIZED => {
             println!("Need to grab a new token");
         }
         other => {
-            panic!("Uh oh! Something unexpected happened: {:?}", other);
+            panic!("Uh oh! Something unexpected happened: [{:#?}]", other);
         }
     };
 }
-
-// TODO: discuss error variants https://rust-lang-nursery.github.io/rust-cookbook/errors/handle.html
