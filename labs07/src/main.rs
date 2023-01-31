f9954c133a6f2585b3f4143053922cf1bb510e9e
El Bruno <bcapuano@gmail.com>
2023-01-31 09:27:51 -0500

    labs for kahoot

--- labs07/src/main.rs
+++ labs07/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let fruits = vec!["coco", "banana"];
+    for &index in [1, 0, 5].iter() {
+        match fruits.get(index) {
+            Some(&"coco") => println!("Coco!!!"),
+            Some(fruit_name) => println!("{}", fruit_name),
+            None => println!("No fruta en {}", index),
+        }
+    }
 }
