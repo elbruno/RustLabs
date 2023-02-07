9cd3a9e60fe50fb81db667e62e001940cde1a4ef
El Bruno <bcapuano@gmail.com>
2023-02-07 10:19:31 -0500

    update data type sample

--- datatype01/src/main.rs
+++ datatype01/src/main.rs
@@ -1,11 +1,35 @@
-fn main() {
-    // create new empty vector with generic datatype
-    let vChar = vec!['A','c','e'];
-    let vNum = vec![1,2];
+/*
+   Copyright (c) 2023
+   Author      : Bruno Capuano
+   Create Time : 2023 Feb
+   Change Log  :
+   - Demo to find variables data type
+   
+   The MIT License (MIT)
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
 
-    print_variable_type(&vChar[0]);
-    print_variable_type(&vNum[0]);
-    
+fn main() {
+    let v_char = vec!['A','c','e'];
+    let v_num = vec![1,2];
+
+    print_variable_type(&v_char);
+    print_variable_type(&v_char[0]);
+    print_variable_type(&v_num[0]);    
 }
 
 fn print_variable_type<K>(_: &K) {
